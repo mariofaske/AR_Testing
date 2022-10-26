3e589f53105d3eefe1dc96cdc29359f092228117
Mario Faske <mario.faske@th-koeln.de>
2022-10-26 15:14:31 +0200

    add distance calculations for gps

--- script.js
+++ script.js
@@ -22,7 +22,42 @@ let id = navigator.geolocation.watchPosition(getPosition);
 
 function getPosition(pos) {
   const button = document.querySelector('button[data-action="change"]');
-  button.innerText = pos.coords.latitude + " " + pos.coords.longitude;
+  let distance = calculateDistance(
+    oldLat,
+    oldLong,
+    pos.coords.latitude,
+    pos.coords.longitude
+  );
+  let oldLat = pos.coords.latitude;
+  let oldLong = pos.coords.longitude;
+  let info = `${pos.coords.latitude} :  ${pos.coords.longitude} : ${distance}`;
+  button.innerText = info;
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  // The math module contains a function
+  // named toRadians which converts from
+  // degrees to radians.
+  lon1 = (lon1 * Math.PI) / 180;
+  lon2 = (lon2 * Math.PI) / 180;
+  lat1 = (lat1 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+
+  // Haversine formula
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+  let c = 2 * Math.asin(Math.sqrt(a));
+
+  // Radius of earth in kilometers. Use 3956
+  // for miles
+  let r = 6371;
+
+  // calculate the result
+  return c * r;
 }
 
 function staticLoadPlaces(lat, long) {
